80a10db03ad7d885cfc6941af5f19425c523829b
Alexandre Dulaunoy <a@foo.be>
2011-08-03 17:27:48 +0200

    cleanup dump script

--- tools/pdns-dump/fulldump.sh
+++ tools/pdns-dump/fulldump.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 #
 
-rm fulldump
 FILEDUMP="/home/adulau/fulldump.pdns"
 FILTER="*"
-redis-cli KEYS "s:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDU
-MP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
-redis-cli KEYS "l:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDU
-MP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
-redis-cli KEYS "o:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDU
-MP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
-redis-cli KEYS "r:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDU
-MP}; VAL=`redis-cli --raw -d , SMEMBERS ${LINE}`; echo "$VAL">>${FILEDUMP}; done
-;
+redis-cli KEYS "s:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDUMP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
+redis-cli KEYS "l:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDUMP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
+redis-cli KEYS "o:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDUMP}; VAL=`redis-cli GET ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
 
+redis-cli KEYS "r:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDUMP}; VAL=`redis-cli --raw -d , SMEMBERS ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
 FILTER="*"
-redis-cli KEYS "v:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDU
-MP}; VAL=`redis-cli --raw -d , SMEMBERS ${LINE}`; echo "$VAL">>${FILEDUMP}; done
-;
+redis-cli KEYS "v:${FILTER}" | while read LINE; do echo -n $LINE." = ">>${FILEDUMP}; VAL=`redis-cli --raw -d , SMEMBERS ${LINE}`; echo "$VAL">>${FILEDUMP}; done;
 
 
